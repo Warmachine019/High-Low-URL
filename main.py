dbe7d5e60e291cf594beaeed5a0fc20aa508ee23
Spandan <91448111+Warmachine019@users.noreply.github.com>
2025-02-22 23:51:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Debug is enabled in order for the changes made to the file to be reflected on the webpage without needing to restart the server.
+if __name__ == "__main__":
+    app.run(debug=True)
