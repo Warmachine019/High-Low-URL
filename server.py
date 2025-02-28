a8786b50ac7aae0c6ed5440bfb4815f238e15114
Spandan <91448111+Warmachine019@users.noreply.github.com>
2025-02-28 19:53:22 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,13 +7,11 @@ print(random_no)
 
 app = Flask(__name__)
 
-
 def make_bold(function):
     def wrapper():
         return "<b>" + function() + "</b>"
 
     return wrapper
-
 
 def make_underlined(function):
     def wrapper():
@@ -21,14 +19,12 @@ def make_underlined(function):
 
     return wrapper
 
-
 @app.route("/")
 @make_bold
 def home():
     return ('<h1>Guess any number from 0-100</h1>'
             '<h2>And then type it in the URL bar with a "/"</h2>'
             '<img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcXN3OXR0MDNuMmdxamxka3FpMDJxY3ExZGdpbDQyN2pmOXlnaHdrYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/y65VoOlimZaus/giphy.gif">')
-
 
 @app.route("/<int:guess>")
 def guess_number(guess):
@@ -42,7 +38,6 @@ def guess_number(guess):
     else:
         return ("<h1 style='color: green'>Correctly Guessed!</h1>"
                 "<img src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExanRoOWNvdjlvOTh5b2JiejN2enV0aTlrOHV6bm02dGY0ZmVpYnE1biZlcD12MV9naWZzX3NlYXJjaCZjdD1n/cXblnKXr2BQOaYnTni/giphy.gif'/>")
-
 
 if __name__ == "__main__":
     app.run(debug=False)
